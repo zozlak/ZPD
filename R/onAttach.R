3a0f72d9543d940d61e960f2c40f292e9c6df23e
Mateusz Żółtak <zozlak@zozlak.org>
2015-03-16 09:11:52 +0100

    dodanie weryfikacji wersji pakietu (i ew. komunikatu sugerującego aktualizację) przy ładowaniu pakietu

---
+++ R/onAttach.R
@@ -0,0 +1,34 @@
+.onAttach = function(libname, pkgname){
+  # pobieranie opisu pakietu
+  tmp = tempfile()
+  url = 'https://raw.githubusercontent.com/zozlak/ZPD/master/DESCRIPTION'
+  flaga = FALSE
+  for(metoda in c('internal', 'curl', 'wget', 'lynx')){
+    wynik = try(
+      {
+        suppressMessages(suppressWarnings(
+          download.file(url, destfile = tmp, method = metoda, quiet = TRUE)
+        ))
+        TRUE
+      },
+      silent = TRUE
+    )
+    if(is.logical(wynik)){
+      flaga = TRUE
+      break
+    }
+  }
+
+  # sprawdzanie wersji
+  if(flaga){
+    opis = suppressWarnings(readLines(tmp))
+    unlink(tmp)
+  
+    wersja = opis[grepl('Version', opis)]
+    wersja = gsub('^[^0-9]+', '', wersja)
+
+    if(wersja != packageVersion('ZPD')){
+      warning('\n\nPosiadasz nieaktualna wersje pakietu ZPD\nRozwaz aktualizacje:\n  devtools::install_github("zozlak/ZPD")')
+    }
+  }
+}
