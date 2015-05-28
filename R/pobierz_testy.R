1de5f2b1b31e0c15034deb67cad78c87b9d00d6e
Mateusz Żółtak <zozlak@zozlak.org>
2015-05-28 22:46:47 +0200

    - Zmiany w grupie danych "testy":   - dodanie zmiennej "prefiks"

--- R/pobierz_testy.R
+++ R/pobierz_testy.R
@@ -15,10 +15,12 @@ pobierz_testy = function(
       extract(year FROM COALESCE(a.data_egzaminu, t.data)) AS rok,
       COALESCE(a.data_egzaminu, t.data) AS data_testu, 
       arkusz IS NOT NULL AS czy_egzamin,
-      COALESCE(opis, arkusz) AS opis_testu
+      COALESCE(opis, arkusz) AS opis_testu,
+      prefiks
     FROM 
       testy t
       LEFT JOIN arkusze a USING (arkusz)
+      LEFT JOIN sl_czesci_egzaminow sce ON (COALESCE(t.rodzaj_egzaminu, a.rodzaj_egzaminu), COALESCE(t.czesc_egzaminu, a.czesc_egzaminu))  = (sce.rodzaj_egzaminu, sce.czesc_egzaminu)
 	"
   data = tbl(src, sql(e(query)))
   return(data)
