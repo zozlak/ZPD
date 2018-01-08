01da55426a3f06670080c2d5216d3d9e97623a84
Mateusz Żółtak <zozlak@zozlak.org>
2018-01-08 18:56:27 +0100

    Zmiana backendu na RPostgres (który po drodze wskoczył do CRAN-a)

--- R/polacz.R
+++ R/polacz.R
@@ -15,24 +15,16 @@ polacz = function(
   host = '89.231.23.130',
   port = 5432,
   user = 'ewd_baza',
-  password = 'CalEBo9',
-  backend = 'RPostgreSQL'
+  password = 'CalEBo9'
 ){
-  stopifnot(
-    is.vector(backend), is.character(backend), length(backend) == 1, all(!is.na(backend)), all(backend %in% c('RPostgreSQL', 'RPostgres'))
-  )
-  if (backend == 'RPostgres') {
-    conn = DBI::dbConnect(RPostgres::Postgres(), dbname, host, port, password, user);
-  } else {
-    conn = DBI::dbConnect(RPostgreSQL::PostgreSQL(), user, password, host, dbname, port);
-  }
+  conn = DBI::dbConnect(RPostgres::Postgres(), dbname, host, port, password, user);
   conn = dbplyr::src_dbi(conn, auto_disconnect = TRUE)
   enc = tolower(Sys.getlocale('LC_CTYPE'))
   if (!grepl('utf-8', enc)) {
     # get the encoding name and set client encoding for the connection
     enc = paste0('WIN', sub('^[^0-9]+([0-9]+)$', '\\1', enc))
-    DBI::dbGetQuery(conn$con, sprintf("SET NAMES '%s'", enc))
+    DBI::dbExecute(conn$con, sprintf("SET NAMES '%s'", enc))
   }
-  DBI::dbGetQuery(conn$con, 'SET client_min_messages to WARNING')
+  DBI::dbExecute(conn$con, 'SET client_min_messages to WARNING')
   return(conn)
 }
