ca81071d620cfd92999a9480ad56d7de134dc5c7
Mateusz Żółtak <zozlak@zozlak.org>
2021-05-08 19:35:56 +0200

    Update polacz.R

--- R/polacz.R
+++ R/polacz.R
@@ -6,15 +6,17 @@
 #' @param port port, na którym nasłuchuje serwer baz danych
 #' @param user użytkownik
 #' @param password hasło
+#' @param ... dodatkowe parametry do przekazania do DBI::dbConnect()
 #' @export
 polacz = function(
   dbname = 'ewd',
   host = '194.54.27.158',
   port = 5432,
   user = 'ewd_baza',
-  password = 'CalEBo9'
+  password = 'CalEBo9',
+  ...
 ){
-  conn = DBI::dbConnect(RPostgres::Postgres(), dbname, host, port, password, user, check_interrupts = TRUE);
+  conn = DBI::dbConnect(RPostgres::Postgres(), dbname, host, port, password, user, ...);
   conn = dbplyr::src_dbi(conn, auto_disconnect = TRUE)
   DBI::dbExecute(conn$con, 'SET client_min_messages to WARNING')
   return(conn)
